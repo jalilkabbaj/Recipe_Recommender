888951c0d70a41badf223c29c710b31b196883f9
Abdeljalil Kabbaj <ajk@MacBook-Pro-8.local>
2021-12-23 15:01:36 +0100

    t

--- github_web_app.py
+++ github_web_app.py
@@ -16,6 +16,7 @@ import nltk
 
 import pickle
 
+import utils
 
 ##################################################################################################################################
 class MeanEmbeddingVectorizer(object):
